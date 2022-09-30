4800dc64df3be6d549781f1dfc81490a36460a09
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-09-30 09:49:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/controlesbasicos/SecondInterface.java
@@ -0,0 +1,14 @@
+package com.example.controlesbasicos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
